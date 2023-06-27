48a14ea3d5ebe6e8f6f54122559fa9be12506523
iwin88789 <134831098+iwin88789@users.noreply.github.com>
2023-06-27 16:56:33 +0700

    NewSlots

---
+++ src/settings.e4e95.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"3c089",main:"4f586"}};
